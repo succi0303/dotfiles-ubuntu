927a1b15c096e066982c31851a266f81e251b97a
Takashi Sugimoto <succi0303@gmail.com>
2024-11-01 02:06:42 +0000

    Fix dotfile path

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
-#!bin/bash -e
+#!/bin/bash -e
 
 IGNORE_PATTERN="^\.(git)"
 DOTDIR="$( cd "$( dirname "$0" )" && pwd )"
 
 echo "START create dotfile links."
-for dotfile in $DOTDIR/.??*; do
-  [[ $dotfile =~ $IGNORE_PATTERN ]] && continue
-  ln -snfv "$DOTDIR/$dotfile" "$HOME/$dotfile"
+for dotfilepath in $DOTDIR/.??*; do
+    dotfile = basename dotfilepath
+    [[ dotfile =~ $IGNORE_PATTERN ]] && continue
+    ln -snfv "$dotfilepath" "$HOME/$dotfile"
 done
 echo "END create dotfile links."

d7ff954e4fc0bea37cfd58c5485a94cdb2177ee7
Takashi Sugimoto <succi0303@gmail.com>
2024-11-01 01:45:24 +0000

    Set DOTDIR

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!bin/bash -e
 
 IGNORE_PATTERN="^\.(git)"
+DOTDIR="$( cd "$( dirname "$0" )" && pwd )"
 
 echo "START create dotfile links."
-for dotfile in .??*; do
+for dotfile in $DOTDIR/.??*; do
   [[ $dotfile =~ $IGNORE_PATTERN ]] && continue
   ln -snfv "$(pwd)/$dotfile" "$HOME/$dotfile"
 done
